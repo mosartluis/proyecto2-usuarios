36dd1914dd55729b525cf6dd5fd2740d633a1669
mosartluis <mosart.luis@gmail.com>
2023-02-01 18:03:12 -0500

    validaciones ok

--- src/app.js
+++ src/app.js
@@ -1,5 +1,50 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let baseID = 0;
+
+const usersDB = [
+    {
+        id: baseID++,
+        firstName: 'Sahid',
+        lastName: 'Kick',
+        email: 'sahid.kick@academlo.com',
+        password: 'root',
+        age: 22
+    }
+];
+
+// CREATE USERS
+app.post("/users", (req, res) => {
+    const formatUser = {
+        id: baseID++,
+        firstName: req.body?.firstName,
+        lastName: req.body?.lastName,
+        email: req.body?.email,
+        password: req.body?.password,
+        age: req.body?.age
+    };
+    usersDB.push(formatUser);
+    res.status(201).send(formatUser);
+});
 
 
-module.exports = app
+// VIEW USERS
+app.get("/users", (req, res) => {
+    res.status(200).send(usersDB)
+});
+
+//? VIEW USERS BY ID
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedUsers = usersDB.find(user => user.id == id);
+    res.status(200).send(selectedUsers)
+});
+
+app.listen(9000, () => {
+    console.log('Server is run');
+});
+
+module.exports = app;
